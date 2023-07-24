b74199a580f81d219f90c1c122ba0199bc7f68ef
Sait <saittaskin2402@gmail.com>
2023-07-25 01:20:45 +0300

    ilk commit

---
+++ src/day_01/C01.java
@@ -0,0 +1,6 @@
+package day_01;
+
+public class C01 {
+
+    /// C:\Users\İzzet Pc\Desktop\GitHubDersi
+}
---
+++ src/day_01/C02.java
@@ -0,0 +1,15 @@
+package day_01;
+
+public class C02 {
+    /*
+    git.init komutu, bir workingDirectory'u git ilr irtibatlandırır
+    bu komut her proje için sadece 1 kere çalıstırılır.
+    bu komut ile birlikte git içinde bulunan klasoru kontrol etmeye baslar
+
+    Bu klasorde her yedek almak ıstedıgımızde commit yapmalayız
+    asgıdakı 3 komut bızım dosyalarımızı her yedeklemek ıstedıgımız de kullanılır
+
+    1- Git add .  : son kaydedeilen versıyondan sonra projede yapılan degısıklerı belırler
+
+     */
+}
